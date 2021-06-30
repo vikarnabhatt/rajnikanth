00c41732a942453712d39947474d87fb788a93c2
vikarnabhatt <vikarnabhatt@gmail.com>
2021-06-30 22:26:50 +0530

    Initial version

---
+++ cowin-pincode.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+center = urlopen('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=400064&date=30-06-2021')
+center_words = []
+for line in center:
+	story_words = line.decode('utf-8').split()
+	for word in story_words:
+		center_words.append(word)
+center.close()
+print (center_words)
